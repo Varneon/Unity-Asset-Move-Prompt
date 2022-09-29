ce6e0d8c04779a43e1ad043d23f79166fe922957
Varneon <me@varneon.com>
2022-09-29 10:55:33 -0700

    Add package files

---
+++ Packages/com.varneon.asset-move-prompt/Editor/AssetMovePrompt.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEditor;
+
+namespace Varneon.AssetMovePrompt
+{
+    /// <summary>
+    /// Asset modification processor class for verifying if user intended to move assets in the project
+    /// </summary>
+    public class AssetMovePrompt : UnityEditor.AssetModificationProcessor
+    {
+        /// <summary>
+        /// The time window for completing all asset move actions
+        /// </summary>
+        private static DateTime modificationTimeWindow = DateTime.MinValue;
+
+        /// <summary>
+        /// Cached move result
+        /// </summary>
+        private static AssetMoveResult moveResult;
+
+        private static AssetMoveResult OnWillMoveAsset(string sourcePath, string destinationPath)
+        {
+            // If this method gets invoked withing the time window, return the cached result
+            if(DateTime.Now < modificationTimeWindow) { return moveResult; }
+
+            // Check if the user intended to move the assets
+            bool allowedToMoveAssets = EditorUtility.DisplayDialog("Move asset(s)?", "Are you sure you want to move asset(s)?", "Yes", "No");
+
+            // Assign the cached move result
+            moveResult = allowedToMoveAssets ? AssetMoveResult.DidNotMove : AssetMoveResult.FailedMove;
+
+            // Add one second to the time window of the current batch modification
+            modificationTimeWindow = DateTime.Now.AddSeconds(1);
+
+            return moveResult;
+        }
+    }
+}
